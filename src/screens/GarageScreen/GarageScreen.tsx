b0d8afee989eceb3d0e4245eea77377ee83cee10
Felipe <104037790+LippeTK@users.noreply.github.com>
2025-08-22 14:05:57 -0300

    First commit

---
+++ src/screens/GarageScreen/GarageScreen.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./GarageScreenStyles";
+
+export default function GarageScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraBox}>
+        <View style={styles.triangleCorner}></View>
+        <View style={[styles.triangleCorner, styles.topRight]}></View>
+        <View style={[styles.triangleCorner, styles.bottomRight]}></View>
+        <View style={[styles.triangleCorner, styles.bottomLeft]}></View>
+      </View>
+    </View>
+  );
+}
