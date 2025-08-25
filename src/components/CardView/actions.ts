5658d6065ea7240ffc041ca7d9d7baaa6a3caa9b
Felipe Carvalho <104037790+LippeTK@users.noreply.github.com>
2025-08-25 16:11:16 -0300

    Feat: creating car model

---
+++ src/components/CardView/actions.ts
@@ -0,0 +1,6 @@
+//faz o request pra api
+export const loadCarData = async () => {}
+
+export const handlePreviousItem = async () => {}
+export const handleNextItem = async () => {}
+export const handleBuy = async () => {}
